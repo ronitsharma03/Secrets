955598e322b676155fa03b897c1e2b50f32dc255
Ronit khajuria <94978319+ronitsharma03@users.noreply.github.com>
2023-04-23 04:16:37 +0530

    updated index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ passport.deserializeUser(function (user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://secrets-sanctum-app.onrender.com/auth/google/secrets"
 },
     (accessToken, refreshToken, profile, cb) => {
         googleUser = profile._json.name;
